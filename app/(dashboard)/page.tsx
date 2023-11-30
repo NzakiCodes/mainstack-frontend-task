714fcaadce1f249f85bd828003a29eaa436661c3
Ekikere-abasi Ekere <ekisnzaki@gmail.com>
2023-11-30 23:23:16 +0100

    Added Wallet API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,13 +6,14 @@ import AvailableBalance from "./balance/available-balance";
 import Transactions from "./transactions";
 import TransactionsChart from "./transactions/chart";
 import { useEffect, useState } from "react";
-import { fetchTransactions, fetchUser } from "@/api";
-import { TransactionsType } from "@/interfaces";
+import { fetchTransactions, fetchUser, fetchWallet } from "@/api";
+import { TransactionsType, Wallet } from "@/interfaces";
 
 export default function Home() {
   const [transactionData, setTransactionData] = useState<
     TransactionsType[] | undefined
   >();
+  const [walletData, setWalletData] = useState<Wallet | undefined>();
   const [loading, setLoading] = useState<boolean>(false);
 
   const getTransactions = async () => {
@@ -26,11 +27,11 @@ export default function Home() {
       throw error;
     }
   };
-  const getUser = async () => {
+  const getWallet = async () => {
     try {
       setLoading(true);
-      const res = (await fetchUser()).data;
-      setTransactionData(res);
+      const res = (await fetchWallet()).data;
+      setWalletData(res);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -40,20 +41,27 @@ export default function Home() {
 
   useEffect(() => {
     getTransactions();
+    getWallet();
   }, []);
 
   return (
     <Box as="main" bgColor={"white"} w={"full"}>
       <Flex columnGap={124} justifyContent={"space-between"} my={16}>
         <Box w={765}>
-          <AvailableBalance balance={120_500} />
+          <AvailableBalance
+            balance={!loading && walletData ? walletData?.balance : 0}
+          />
           <TransactionsChart />
         </Box>
         <Balances
-          ledger_balance={0.0}
-          total_payout={120_000}
-          total_revenue={200}
-          pending_payout={0}
+          ledger_balance={
+            !loading && walletData ? walletData?.ledger_balance : 0
+          }
+          total_payout={!loading && walletData ? walletData?.total_payout : 0}
+          total_revenue={!loading && walletData ? walletData?.total_revenue : 0}
+          pending_payout={
+            !loading && walletData ? walletData?.pending_payout : 0
+          }
         />
       </Flex>
       <Transactions
